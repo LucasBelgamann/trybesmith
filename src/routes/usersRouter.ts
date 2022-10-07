108bbe9dbb6973b05911065ccd07c65bb88bdc4e
LucasBelgamann <lucasbelgamann@hotmail.com>
2022-10-07 16:42:08 -0300

    feat: requisito 7 completo

--- src/routes/usersRouter.ts
+++ src/routes/usersRouter.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express';
 import UserController from '../controller/User.controller';
+import validateUsers from '../middlewares/validateUser';
 
 const router = Router();
 
 const UserControllerr = new UserController();
 
-router.post('/', UserControllerr.create);
+router.post(
+  '/',
+  validateUsers.validateClasse,
+  validateUsers.validateLevel,
+  validateUsers.validatePassword,
+  validateUsers.validateUsername,
+  UserControllerr.create,
+);
 
 export default router;
